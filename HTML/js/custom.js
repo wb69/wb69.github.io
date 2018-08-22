a50b3a72e195650ee95b8034445b0bff2900d5ef
wb69 <wb69@student.le.ac.uk>
2018-08-22 01:56:35 +0100

    Used template for starters

---
+++ HTML/js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	// Scroll Down Link
+	$('.scrolldown').click(function() {
+		var targetOffset = $('div.blockquote').offset().top-80;
+		$('html,body').animate({scrollTop: targetOffset}, 1000);
+	});
+
+	// Text Rotator
+	$('.rotate').each(function(){
+		var el = $(this);
+		var text = $(this).html().split(",");
+		el.html(text[0]);
+		setInterval(function() {
+			el.animate({
+              textShadowBlur:20,
+              opacity: 0
+            }, 500 , function() {
+              index = $.inArray(el.html(), text)
+              if((index + 1) == text.length) index = -1
+              el.text(text[index + 1]).animate({
+                textShadowBlur:0,
+                opacity: 1
+              }, 500 );
+            });
+		}, 2000);
+	});
+
+});
